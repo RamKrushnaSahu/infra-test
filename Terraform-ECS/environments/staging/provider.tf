43be575dbb103c222cfb04f9b62d710c90ee2765
Ram Krushna Sahu <sahuramkrushna1997@gmail.com>
2022-06-29 00:36:12 +0530

    AWSQuarantine policy removed in console

--- Terraform-ECS/environments/staging/provider.tf
+++ Terraform-ECS/environments/staging/provider.tf
@@ -1,16 +1,16 @@
 ## Provider
 
+/*
 provider "aws" {
   region     = "ap-south-1"
   access_key = "AKIA2ILO7YHVF5YLJV64"
   secret_key = "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"
 }
+*/
 
 
-/*
 provider "aws" {
   region     = "ap-south-1"
   access_key = "AKIA2ILO7YHVDWVI5K6V"
   secret_key = "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"
 }
-*/
